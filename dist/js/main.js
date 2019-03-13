d0c97b544638a8cc438918a77b23267be4022c51
ariqshd <invanteris@gmail.com>
2019-03-13 20:25:28 +0700

    Text color function & about page

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,9 +3,9 @@ const menuBtn = document.querySelector(".menu-btn");
 const menu = document.querySelector(".menu");
 const menuNav = document.querySelector(".menu-nav");
 const menuBranding = document.querySelector(".menu-branding");
-const navItem = document.querySelectorAll(".nav-items");
+const navItems = document.querySelectorAll(".nav-item");
 
-// Set Initia State Of Menu
+// Set Initial State Of Menu
 let showMenu = false;
 
 menuBtn.addEventListener("click", toggleMenu);
@@ -16,7 +16,7 @@ function toggleMenu() {
     menu.classList.add("show");
     menuNav.classList.add("show");
     menuBranding.classList.add("show");
-    navItem.forEach(item => item.classList.add("show"));
+    navItems.forEach(item => item.classList.add("show"));
 
     // Set Menu State
     showMenu = true;
@@ -25,7 +25,7 @@ function toggleMenu() {
     menu.classList.remove("show");
     menuNav.classList.remove("show");
     menuBranding.classList.remove("show");
-    navItem.forEach(item => item.classList.remove("show"));
+    navItems.forEach(item => item.classList.remove("show"));
 
     // Set Menu State
     showMenu = false;
